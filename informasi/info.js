2c6232782c14ad2f44605b7bd82408261f7f381f
NasrulNgeGithub <nasrul.matc@gmail.com>
2024-05-04 21:34:48 +0800

    Add files via upload

--- informasi/info.js
+++ informasi/info.js
@@ -1,1 +1,8 @@
-
+TargetDate = "05/05/2023 01:00 PM";
+BackColor = "";
+ForeColor = "";
+CountActive = true;
+CountStepper = -1;
+LeadingZero = true;
+DisplayFormat = "Hitung Mundur Menuju Informasi Kelulusan SMA YAPIP Makassar Sungguminasa (05 Mei 2023 Pukul 13:00 WITA)<br/>%%D%% Hari, %%H%% Jam, %%M%% Menit, %%S%% Detik";
+FinishMessage = "<b>Informasi:</b> Hore, sekarang sudah waktunya pengumuman! Klik button untuk melihat Informasi Kelulusan 2023 <a href='https://goo.gl/MC5dMi' class='button large red'>Lihat Informasi Kelulusan</a>";

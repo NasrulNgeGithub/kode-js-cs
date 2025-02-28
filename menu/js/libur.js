149bade04fa5d665d48a8627b5161366caf54e44
NasrulNgeGithub <nasrul.matc@gmail.com>
2025-02-28 23:31:25 +0800

    Add files via upload

---
+++ menu/js/libur.js
@@ -0,0 +1,19 @@
+const tahunSekarang = new Date().getFullYear();
+
+const hariLibur = {
+  [`${tahunSekarang}-01-01`]: "Tahun Baru Masehi",
+  [`${tahunSekarang}-01-22`]: "Tahun Baru Imlek",
+  [`${tahunSekarang}-03-22`]: "Hari Suci Nyepi",
+  [`${tahunSekarang}-04-07`]: "Wafat Isa Almasih",
+  [`${tahunSekarang}-04-22`]: "Hari Raya Idul Fitri",
+  [`${tahunSekarang}-04-23`]: "Cuti Bersama Idul Fitri",
+  [`${tahunSekarang}-05-01`]: "Hari Buruh Internasional",
+  [`${tahunSekarang}-05-18`]: "Kenaikan Isa Almasih",
+  [`${tahunSekarang}-06-01`]: "Hari Lahir Pancasila",
+  [`${tahunSekarang}-06-04`]: "Hari Waisak",
+  [`${tahunSekarang}-06-29`]: "Hari Raya Idul Adha",
+  [`${tahunSekarang}-07-19`]: "Tahun Baru Islam",
+  [`${tahunSekarang}-08-17`]: "Hari Kemerdekaan RI",
+  [`${tahunSekarang}-09-28`]: "Maulid Nabi Muhammad SAW",
+  [`${tahunSekarang}-12-25`]: "Hari Raya Natal",
+};

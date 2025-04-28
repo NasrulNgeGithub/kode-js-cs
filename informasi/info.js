f4eb88a2f80c6413865404b50ae971d1e5ee37d6
ᨉᨕᨙ ᨆᨔᨗᨁ <nasrul.matc@gmail.com>
2025-04-28 22:57:37 +0800

    Update info.js

--- informasi/info.js
+++ informasi/info.js
@@ -1,8 +1,8 @@
-TargetDate = "05/06/2024 01:00 PM";
+TargetDate = "05/05/2025 01:00 PM";
 BackColor = "";
 ForeColor = "";
 CountActive = true;
 CountStepper = -1;
 LeadingZero = true;
-DisplayFormat = "Hitung Mundur Menuju Informasi Kelulusan SMA YAPIP Makassar Sungguminasa (06 Mei 2024 Pukul 13:00 WITA)<br/>%%D%% Hari, %%H%% Jam, %%M%% Menit, %%S%% Detik";
-FinishMessage = "<b>Informasi:</b> Hore, sekarang sudah waktunya pengumuman! Klik button untuk melihat Informasi Kelulusan 2024 <a href='https://goo.gl/MC5dMi' class='button large red'>Lihat Informasi Kelulusan</a>";
+DisplayFormat = "Hitung Mundur Menuju Informasi Kelulusan SMA YAPIP Makassar Sungguminasa (05 Mei 2025 Pukul 13:00 WITA)<br/>%%D%% Hari, %%H%% Jam, %%M%% Menit, %%S%% Detik";
+FinishMessage = "<b>Informasi:</b> Hore, sekarang sudah waktunya pengumuman! Klik button untuk melihat Informasi Kelulusan 2025 <a href='https://goo.gl/MC5dMi' class='button large red'>Lihat Informasi Kelulusan</a>";

286a0b6b114950eae381cab178dedc37714c9116
ᨉᨕᨙ ᨆᨔᨗᨁ <nasrul.matc@gmail.com>
2025-06-10 20:55:06 +0800

    Update and rename datan2023.js to datan.js

---
+++ media/datan/datan.js
@@ -0,0 +1,99 @@
+// data2024
+const studentsData2024 = [
+    {
+        id: 'budi_2024',
+        name: 'Budi Santoso',
+        nis: '1024001',
+        nisn: '90010001',
+        class: '3A',
+        peminatan: 'Ilmu Pengetahuan Alam',
+        grades: {
+            s1: {
+                'Matematika': { kog: 85, psik: 75 },
+                'Bahasa Indonesia': { kog: 90, psik: 80 },
+                'IPA': { kog: 82, psik: 78 }
+            },
+            s2: {
+                'Matematika': { kog: 88, psik: 78 },
+                'Bahasa Indonesia': { kog: 85, psik: 82 },
+                'IPA': { kog: 90, psik: 85 }
+            },
+            s3: {
+                'Matematika': { kog: 82, psik: 76 },
+                'Bahasa Indonesia': { kog: 88, psik: 80 },
+                'IPA': { kog: 85, psik: 81 }
+            },
+            s4: {
+                'Matematika': { kog: 90, psik: 80 },
+                'Bahasa Indonesia': { kog: 92, psik: 88 },
+                'IPA': { kog: 88, psik: 84 }
+            },
+            s5: {
+                'Matematika': { kog: 87, psik: 79 },
+                'Bahasa Indonesia': { kog: 89, psik: 85 },
+                'IPA': { kog: 90, psik: 86 }
+            },
+            s6: {
+                'Matematika': { kog: 89, psik: 81 },
+                'Bahasa Indonesia': { kog: 91, psik: 87 },
+                'IPA': { kog: 92, psik: 88 }
+            },
+            nus: {
+                'Matematika': 80,
+                'Bahasa Indonesia': 85,
+                'IPA': 82
+            }
+        }
+    },
+    // ... siswa lain untuk tahun 2024 ...
+];
+
+// data2025
+const studentsData2025 = [
+    {
+        id: 'jaka_2025',
+        name: 'Jaka Permana',
+        nis: '1025001',
+        nisn: '90020001',
+        class: '3A',
+        peminatan: 'Ilmu Pengetahuan Alam',
+        grades: {
+            s1: {
+                'Matematika': { kog: 78, psik: 70 },
+                'Bahasa Indonesia': { kog: 80, psik: 72 },
+                'IPA': { kog: 75, psik: 68 }
+            },
+            s2: {
+                'Matematika': { kog: 80, psik: 72 },
+                'Bahasa Indonesia': { kog: 82, psik: 74 },
+                'IPA': { kog: 78, psik: 70 }
+            },
+            s3: {
+                'Matematika': { kog: 75, psik: 68 },
+                'Bahasa Indonesia': { kog: 78, psik: 70 },
+                'IPA': { kog: 73, psik: 65 }
+            },
+            s4: {
+                'Matematika': { kog: 82, psik: 74 },
+                'Bahasa Indonesia': { kog: 84, psik: 76 },
+                'IPA': { kog: 80, psik: 72 }
+            },
+            s5: {
+                'Matematika': { kog: 79, psik: 71 },
+                'Bahasa Indonesia': { kog: 81, psik: 73 },
+                'IPA': { kog: 77, psik: 69 }
+            },
+            s6: {
+                'Matematika': { kog: 81, psik: 73 },
+                'Bahasa Indonesia': { kog: 83, psik: 75 },
+                'IPA': { kog: 79, psik: 71 }
+            },
+            nus: {
+                'Matematika': 70,
+                'Bahasa Indonesia': 75,
+                'IPA': 72
+            }
+        }
+    },
+    // ... siswa lain untuk tahun 2025 ...
+];
